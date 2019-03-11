5f52449ad32ab49b762a0eb4b0010ef2056c6895
Anil Maskara <Anil.Maskara@capgemini.com>
2019-03-11 11:45:47 +0530

    First Commit

---
+++ src/webparts/devOpsTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDevOpsTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DevOpsTestWebPartStrings' {
+  const strings: IDevOpsTestWebPartStrings;
+  export = strings;
+}
